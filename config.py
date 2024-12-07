2295fd08131f5e203bdcb7b022b8452a3c915443
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-12-07 23:45:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import os
 
 #Database 
 #Database [https://youtu.be/qFB0cFqiyOM?si=fVicsCcRSmpuja1A]
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://muiz123:muiz123AHMED@cluster0.x8pxe.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "cphdlust")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Sunilvs2024:Sunilvs2024@cluster0.fdkbx.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Sunilvs2024")
 
 #Shortner (token system) 
 # check my discription to help by using my refer link of shareus.io
 
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "1ba76e07a358d81f92aebbfe5fce664c17e5b2f3")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "20bb8e8e7b6fb1ccfa4165aa4b55036c44f75ced")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/ultroid_official/18") # shareus ka tut_vid he 
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/how_to_download_SvFilmsX/17") # shareus ka tut_vid he 
