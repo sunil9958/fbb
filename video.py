ceb83edcab98e774782cebb9b09fb6981d757d86
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2024-12-29 23:41:46 +0530

    Update video.py

--- video.py
+++ video.py
@@ -16,7 +16,7 @@ aria2 = aria2p.API(
     )
 )
 async def download_video(url, reply_msg, user_mention, user_id):
-    response = requests.get(f"https://teraboxpremium.itz-ashlynn.workers.dev/?url={url}")
+    response = requests.get(f"http://ashlynn.serv00.net/terapre.php/?url={url}")
     response.raise_for_status()
     data = response.json()
 
